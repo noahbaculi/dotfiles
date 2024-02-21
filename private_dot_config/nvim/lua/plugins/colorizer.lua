fe32587c69d020395f5dd91dd7194599d21e99c1
noahbaculi <nbaculi@carium.com>
2024-02-20 19:30:04 -0800

    add nvim colorizer plugin

---
+++ private_dot_config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,11 @@
+return {
+	"NvChad/nvim-colorizer.lua",
+	commit = "85855b38011114929f4058efc97af1059ab3e41d",
+	event = "VeryLazy",
+	opts = {
+		user_default_options = {
+			-- Available modes for `mode`: foreground, background,  virtualtext
+			mode = "virtualtext", -- Set the display mode.
+		},
+	},
+}

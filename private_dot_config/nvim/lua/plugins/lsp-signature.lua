1cece30c6ac3fe0d4d1f0c6e6417917bedb38e1e
noahbaculi <nbaculi@carium.com>
2024-02-20 16:50:09 -0800

    add lsp_signature commit hash

--- private_dot_config/nvim/lua/plugins/lsp-signature.lua
+++ private_dot_config/nvim/lua/plugins/lsp-signature.lua
@@ -1,8 +1,9 @@
 return {
-  "ray-x/lsp_signature.nvim",
-  event = "VeryLazy",
-  opts = {},
-  config = function(_, opts)
-    require("lsp_signature").setup(opts)
-  end,
+	"ray-x/lsp_signature.nvim",
+	commit = "fed2c8389c148ff1dfdcdca63c2b48d08a50dea0",
+	event = "VeryLazy",
+	opts = {},
+	config = function(_, opts)
+		require("lsp_signature").setup(opts)
+	end,
 }

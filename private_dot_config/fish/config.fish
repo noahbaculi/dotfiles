5e2fd8afab6ee99da08456660d7d37a3b9d27e02
noahbaculi <nbaculi@carium.com>
2024-03-15 09:32:21 -0700

    add ripgrep and tokei packages

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,8 +2,12 @@
 abbr n 'nvim'
 abbr gg 'lazygit'
 
+# Functions
+
+# Source
 source "$HOME/.cargo/env.fish"
 mise activate fish | source
 zoxide init fish | source
 starship init fish | source
 atuin init fish | source
+

193d92277b989ee2e75e12b83b807dba2d38c50d
Noah Baculi <noahbaculi@gmail.com>
2024-05-20 07:28:23 -0700

    add icon-picker plugin

---
+++ private_dot_config/nvim/lua/plugins/icon-picker.lua
@@ -0,0 +1,6 @@
+return {
+  "ziontee113/icon-picker.nvim",
+  commit = "3ee9a0ea9feeef08ae35e40c8be6a2fa2c20f2d3",
+  cmd = { "IconPickerNormal", "IconPickerInsert", "IconPickerYank" },
+  opts = { disable_legacy_commands = true },
+}

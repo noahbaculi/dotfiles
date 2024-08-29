d1dfc64cb4ff9291f172291914606d1bf8dac6b2
Noah Baculi <noahbaculi@gmail.com>
2024-08-28 19:38:05 -0700

    add prettier formatter for html, css, js

--- private_dot_config/nvim/lua/plugins/conform__formatter.lua
+++ private_dot_config/nvim/lua/plugins/conform__formatter.lua
@@ -5,6 +5,9 @@ return {
       -- Use a sub-list to run only the first available formatter
       lua = { "stylua", stop_after_first = true },
       markdown = { "mdformat", stop_after_first = true },
+      html = { "prettier", stop_after_first = true },
+      css = { "prettier", stop_after_first = true },
+      javascript = { "prettier", stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,

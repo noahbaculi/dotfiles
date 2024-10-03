3d38eceed64d7d81fcb13a8ce78c583b733d459a
Noah Baculi <noahbaculi@gmail.com>
2024-10-03 13:56:01 -0700

    update default themes

--- private_dot_config/nvim/lua/plugins/auto-dark-mode.lua
+++ private_dot_config/nvim/lua/plugins/auto-dark-mode.lua
@@ -4,14 +4,16 @@ return {
   config = {
     update_interval = 1e3, -- milliseconds
     set_dark_mode = function()
-      -- vim.api.nvim_set_option("background", "dark")
       vim.opt.background = "dark"
-      vim.cmd.colorscheme("nightfox")
+
+      -- vim.cmd.colorscheme("nightfox")
+      vim.cmd.colorscheme("everforest")
     end,
     set_light_mode = function()
-      -- vim.api.nvim_set_option("background", "light")
       vim.opt.background = "light"
-      vim.cmd.colorscheme("material-lighter")
+
+      -- vim.cmd.colorscheme("material-lighter")
+      vim.cmd.colorscheme("catppuccin-latte")
     end,
   },
 }

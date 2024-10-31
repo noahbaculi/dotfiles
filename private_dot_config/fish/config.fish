cd08a08bf194a866e1e6e28885dd1a2fcea601d0
Noah Baculi <noahbaculi@gmail.com>
2024-10-31 14:39:34 -0700

    wrap fish sourcing to ensure interactive use

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -4,12 +4,14 @@ abbr gg 'lazygit'
 
 # Functions
 
-# Source
-source "$HOME/.cargo/env.fish"
-mise activate fish | source
-zoxide init fish | source
-starship init fish | source
-atuin init fish | source
+if status --is-interactive
+    # Source
+    source "$HOME/.cargo/env.fish"
+    mise activate fish | source
+    zoxide init fish | source
+    starship init fish | source
+    atuin init fish | source
+end
 
 # Rust ESP32 environment variables
 # source "$HOME/export-esp.sh"

ad841cb76eb5abacd40c7aff696545836c1bf8c4
Noah Baculi <noahbaculi@gmail.com>
2024-11-03 07:50:45 -0800

    add yazi config

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -4,14 +4,25 @@ abbr gg 'lazygit'
 
 # Functions
 
-if status --is-interactive
-    # Source
-    source "$HOME/.cargo/env.fish"
-    mise activate fish | source
-    zoxide init fish | source
-    starship init fish | source
-    atuin init fish | source
+## File browse with yazi. Press y to start browsing, press q to quit and change the CWD, press Q to quit without changing the CWD.
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
+
+# Source
+source "$HOME/.cargo/env.fish"
+mise activate fish | source
+zoxide init fish | source
+starship init fish | source
+atuin init fish | source
+
+
+set -gx EDITOR nvim
 
 # Rust ESP32 environment variables
 # source "$HOME/export-esp.sh"

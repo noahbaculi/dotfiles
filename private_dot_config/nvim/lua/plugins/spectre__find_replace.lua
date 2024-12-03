fd5d100f4d325fb60e1bca29f6a77ef71456298c
Noah Baculi <noahbaculi@gmail.com>
2024-12-03 11:18:55 -0800

    update spectre mac os fix

--- private_dot_config/nvim/lua/plugins/spectre__find_replace.lua
+++ private_dot_config/nvim/lua/plugins/spectre__find_replace.lua
@@ -1,4 +1,14 @@
 return {
   "nvim-pack/nvim-spectre",
   cmd = { "Spectre" },
+  replace_engine = {
+    ["sed"] = {
+      cmd = "sed",
+      args = {
+        "-i",
+        "",
+        "-E",
+      },
+    },
+  },
 }

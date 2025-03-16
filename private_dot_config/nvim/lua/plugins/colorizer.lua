f55aa4d8baee651317a5d5dcdd7f7041dc5a38b7
Noah Baculi <noahbaculi@gmail.com>
2025-03-15 19:14:41 -0700

    mv to updated colorizer repo and only init on command

--- private_dot_config/nvim/lua/plugins/colorizer.lua
+++ private_dot_config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,6 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  event = "VeryLazy",
+  "catgoose/nvim-colorizer.lua",
+  cmd = { "ColorizerToggle" },
   opts = {
     user_default_options = {
       -- Available modes for `mode`: foreground, background,  virtualtext

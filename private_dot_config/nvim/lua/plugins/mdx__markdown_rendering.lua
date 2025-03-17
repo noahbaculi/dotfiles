e902cc31d224d00e8f90c82274f45fc51b05811e
Noah Baculi <noahbaculi@gmail.com>
2025-03-17 13:17:44 -0700

    init mdx plugin

---
+++ private_dot_config/nvim/lua/plugins/mdx__markdown_rendering.lua
@@ -0,0 +1,7 @@
+-- This is used for highlighting in .mdx files.
+-- Used for EDB AIDB docs. March 2025.
+return {
+  "davidmh/mdx.nvim",
+  config = true,
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+}

33031cc91e88235981eaecc7ad6c342785561a89
Noah Baculi <noahbaculi@gmail.com>
2025-08-05 11:05:04 -0700

    add lsp actions to crates nvim plugin

--- private_dot_config/nvim/lua/plugins/crates__rust_cargo.lua
+++ private_dot_config/nvim/lua/plugins/crates__rust_cargo.lua
@@ -2,5 +2,16 @@ return {
   "saecki/crates.nvim",
   tag = "stable",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    lsp = {
+      enabled = true,
+      on_attach = function(client, bufnr)
+        -- the same on_attach function as for your other language servers
+        -- can be ommited if you're using the `LspAttach` autocmd
+      end,
+      actions = true,
+      completion = true,
+      hover = true,
+    },
+  },
 }

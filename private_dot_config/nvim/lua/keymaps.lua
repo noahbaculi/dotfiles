76039dc1e09de20cf8bc89145c6b04e7f0dc0ac2
Noah Baculi <noahbaculi@gmail.com>
2026-03-24 10:05:47 -0700

    move gitsigns and lazygit keymaps to plugin spec

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -87,19 +87,6 @@ vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>write<cr>", { desc = "Save buff
 --
 --
 which_key.add({ "<leader>g", group = "Git", icon = "󰊢" })
-vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-
-vim.keymap.set("n", "]g", function() require("gitsigns").next_hunk() end, { desc = "Next Git hunk" })
-vim.keymap.set("n", "[g", function() require("gitsigns").prev_hunk() end, { desc = "Previous Git hunk" })
-vim.keymap.set("n", "<leader>gl", function() require("gitsigns").blame_line() end, { desc = "View Git blame" })
-vim.keymap.set("n", "<leader>gL", function() require("gitsigns").blame_line({ full = true }) end, { desc = "View full Git blame" })
-vim.keymap.set("n", "<leader>gp", function() require("gitsigns").preview_hunk() end, { desc = "Preview Git hunk" })
-vim.keymap.set("n", "<leader>gh", function() require("gitsigns").reset_hunk() end, { desc = "Reset Git hunk" })
-vim.keymap.set("n", "<leader>gr", function() require("gitsigns").reset_buffer() end, { desc = "Reset Git buffer" })
-vim.keymap.set("n", "<leader>gs", function() require("gitsigns").stage_hunk() end, { desc = "Stage Git hunk" })
-vim.keymap.set("n", "<leader>gS", function() require("gitsigns").stage_buffer() end, { desc = "Stage Git buffer" })
-vim.keymap.set("n", "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, { desc = "Unstage Git hunk" })
-vim.keymap.set("n", "<leader>gd", function() require("gitsigns").diffthis() end, { desc = "View Git diff" })
 
 --
 --
--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -3,10 +3,26 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     opts = {},
+    keys = {
+      { "]g", function() require("gitsigns").next_hunk() end, desc = "Next Git hunk" },
+      { "[g", function() require("gitsigns").prev_hunk() end, desc = "Previous Git hunk" },
+      { "<leader>gl", function() require("gitsigns").blame_line() end, desc = "View Git blame" },
+      { "<leader>gL", function() require("gitsigns").blame_line({ full = true }) end, desc = "View full Git blame" },
+      { "<leader>gp", function() require("gitsigns").preview_hunk() end, desc = "Preview Git hunk" },
+      { "<leader>gh", function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" },
+      { "<leader>gr", function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" },
+      { "<leader>gs", function() require("gitsigns").stage_hunk() end, desc = "Stage Git hunk" },
+      { "<leader>gS", function() require("gitsigns").stage_buffer() end, desc = "Stage Git buffer" },
+      { "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage Git hunk" },
+      { "<leader>gd", function() require("gitsigns").diffthis() end, desc = "View Git diff" },
+    },
   },
   {
     "kdheepak/lazygit.nvim",
     cmd = { "LazyGit" },
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
 }

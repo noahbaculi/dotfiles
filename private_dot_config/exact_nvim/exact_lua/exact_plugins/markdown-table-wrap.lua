0a02cb11684e3e5f087e9213e410d810d6eb4c02
Noah Baculi <noahbaculi@gmail.com>
2026-08-15 17:55:41 -0700

    feat(nvim): drop markdown-table-wrap, add render-markdown toggle keymap

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/markdown-table-wrap.lua
+++
@@ -1,6 +0,0 @@
--- Wrap long markdown table cells in reader/overlay preview.
-return {
-  "ice345/markdown-table-wrap.nvim",
-  ft = { "markdown" },
-  opts = {},
-}
--- private_dot_config/exact_nvim/exact_lua/exact_plugins/render-markdown.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/render-markdown.lua
@@ -3,6 +3,9 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
   ft = { "markdown" },
+  keys = {
+    { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", ft = "markdown", desc = "Toggle markdown rendering" },
+  },
   opts = {
     file_types = { "markdown" },
     heading = { position = "inline" },
